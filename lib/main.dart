b11a07678462e832058b7f80a55a05e2c5f56469
user <mohamed.reda.007007@gmail.com>
2019-11-24 17:57:38 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flare_flutter/flare_actor.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  String animationName='Smile';
+  void _incrementCounter() {
+
+    setState(() {
+      if(animationName=='Smile'){
+        animationName='Frown';
+      }else{
+        animationName='Smile';
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Container(
+          width: 300,
+          height: 300,
+          child: FlareActor(
+            'assets/smaily.flr',alignment: Alignment.center,
+            fit: BoxFit.contain,
+            animation: animationName,
+          ),
+        )
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
